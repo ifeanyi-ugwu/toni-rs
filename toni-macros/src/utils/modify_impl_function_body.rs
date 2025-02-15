fcbbe47ba1982dc0cdafae8011fd9ff70e9e90da
Antonio <toniko1910@gmail.com>
2025-02-14 23:34:17 -0300

    fix: modifie correct fn

--- toni-macros/src/utils/modify_impl_function_body.rs
+++ toni-macros/src/utils/modify_impl_function_body.rs
@@ -119,6 +119,78 @@ impl VisitMut for ExprModifier {
             }
         }
 
+        if let Expr::MethodCall(method_call) = &mut *expr {
+            if let Expr::Field(expr_field) = &mut *method_call.receiver {
+                if let Member::Named(ident) = &expr_field.member {
+                    let ident_clone = ident.clone();
+                    let method_args_clone = &method_call.args.clone();
+                    let method_call_name = &method_call.method.clone();
+                    for provide_name in &self.provider_names {
+                        if ident_clone == provide_name.0 {
+                            let method_name = method_call_name;
+                            let new_field_name = create_field_struct_name(
+                                &provide_name.1.to_string(),
+                                method_name,
+                            )
+                            .unwrap();
+                            let args = self.put_box_in_expr(method_args_clone.iter());
+                            self.modified_exprs.push((
+                                provide_name.1.clone(),
+                                method_name.clone(),
+                                new_field_name.clone(),
+                            ));
+                            let new_call_fn_expr: Expr = parse_quote! {
+                                self.#new_field_name.execute(vec![#(#args),*])
+                            };
+                            if self.whitout_type {
+                                *expr = new_call_fn_expr;
+                            } else {
+                                let type_inject = match &self.ty {
+                                    Some(ty) => ty,
+                                    None => panic!("Need type"),
+                                };
+                                let with_downcast: Expr = parse_quote! {
+                                    *#new_call_fn_expr.await.downcast::<#type_inject>().unwrap()
+                                };
+                                *expr = with_downcast;
+                            }
+                            return;
+                        }
+                    }
+                }
+            } else if let Expr::Path(expr_path) = &*method_call.receiver {
+                if let Some(segment) = expr_path.path.segments.last() {
+                    if segment.ident == "self" {
+                        let method_args_clone = method_call.args.clone();
+                        let method_name = method_call.method.clone();
+                        let new_method_name =
+                            create_field_struct_name(&self.self_name.to_string(), &method_name)
+                                .unwrap();
+                        let args = self.put_box_in_expr(method_args_clone.iter());
+                        self.modified_exprs.push((
+                            self.self_name.clone(),
+                            method_name.clone(),
+                            new_method_name.clone(),
+                        ));
+                        let new_call_fn_expr: Expr = parse_quote! {
+                            self.#new_method_name.execute(vec![#(#args),*])
+                        };
+                        if self.whitout_type {
+                            *expr = new_call_fn_expr;
+                            return;
+                        }
+                        let type_inject = match &self.ty {
+                            Some(ty) => ty,
+                            None => panic!("Need type"),
+                        };
+                        let downcast_token: Expr = parse_quote! {
+                            downcast::<#type_inject>().unwrap()
+                        };
+                        *expr = parse_quote! {*#new_call_fn_expr.await.#downcast_token};
+                    }
+                }
+            }
+        }
         visit_mut::visit_expr_mut(self, expr);
     }
 }
