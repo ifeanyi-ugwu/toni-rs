4f90ed59d1a77f6526a31b36fa716e97bc6f2006
Antonio <toniko1910@gmail.com>
2025-02-18 18:09:57 -0300

    feat: functions to ger enhancers metadatas

--- toni/src/traits_helpers/controller.rs
+++ toni/src/traits_helpers/controller.rs
@@ -5,14 +5,17 @@ use rustc_hash::FxHashMap;
 
 use crate::http_helpers::{HttpMethod, HttpRequest, HttpResponse, IntoResponse};
 
-use super::provider::ProviderTrait;
+use super::{provider::ProviderTrait, Guard, Interceptor, Pipe};
 
 #[async_trait]
 pub trait ControllerTrait: Send + Sync {
     fn get_token(&self) -> String;
-    async fn execute(&self, req: HttpRequest) -> Box<dyn IntoResponse<Response = HttpResponse>>;
+    async fn execute(&self, req: HttpRequest) -> Box<dyn IntoResponse<Response = HttpResponse> + Send>;
     fn get_path(&self) -> String;
     fn get_method(&self) -> HttpMethod;
+    fn get_guards(&self) -> Vec<Arc<dyn Guard>>;
+    fn get_pipes(&self) -> Vec<Arc<dyn Pipe>>;
+    fn get_interceptors(&self) -> Vec<Arc<dyn Interceptor>>;
 }
 pub trait Controller {
     fn get_all_controllers(
