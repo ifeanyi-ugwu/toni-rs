39e29b5c4e6a66a5c2fd7a0ae585745ad36029d1
Antonio <toniko1910@gmail.com>
2025-02-18 18:12:05 -0300

    feat: instance wrapper to controller

---
+++ toni/src/injector/instance_wrapper.rs
@@ -0,0 +1,74 @@
+use std::sync::Arc;
+
+use crate::{
+    http_helpers::{HttpMethod, HttpRequest, HttpResponse, IntoResponse},
+    structs_helpers::EnhancerMetadata,
+    traits_helpers::{ControllerTrait, Guard, Interceptor, Pipe}
+};
+
+use super::Context;
+
+pub struct InstanceWrapper {
+    instance: Arc<Box<dyn ControllerTrait>>,
+    guards: Vec<Arc<dyn Guard>>,
+    interceptors: Vec<Arc<dyn Interceptor>>,
+    pipes: Vec<Arc<dyn Pipe>>,
+}
+
+impl InstanceWrapper {
+    pub fn new(
+        instance: Arc<Box<dyn ControllerTrait>>,
+        enhancer_metadata: EnhancerMetadata,
+    ) -> Self {
+        Self {
+            instance,
+            guards: enhancer_metadata.guards,
+            interceptors: enhancer_metadata.interceptors,
+            pipes: enhancer_metadata.pipes,
+        }
+    }
+
+    pub fn get_path(&self) -> String {
+        self.instance.get_path()
+    }
+
+    pub fn get_method(&self) -> HttpMethod {
+        self.instance.get_method()
+    }
+
+    pub async fn handle_request(
+        &self,
+        req: HttpRequest,
+    ) -> Box<dyn IntoResponse<Response = HttpResponse> + Send> {
+        let mut context = Context::from_request(req);
+
+        for guard in &self.guards {
+            if !guard.can_activate(&context) {
+                return context.get_response();
+            }
+        }
+
+        for interceptor in &self.interceptors {
+            interceptor.before_execute(&mut context);
+        }
+
+        for pipe in &self.pipes {
+            pipe.process(&mut context);
+            if context.should_abort() {
+                return context.get_response();
+            }
+        }
+
+        let req = context.take_request().clone();
+
+        let controller_response = self.instance.execute(req).await;
+
+        context.set_response(controller_response);
+
+        for interceptor in &self.interceptors {
+            interceptor.after_execute(&mut context);
+        }
+
+        context.get_response()
+    }
+}
--- toni/src/injector/mod.rs
+++ toni/src/injector/mod.rs
@@ -1,7 +1,15 @@
 mod container;
 pub use self::container::ToniContainer;
+
 mod instance_loader;
 pub use self::instance_loader::ToniInstanceLoader;
 mod module;
+
 mod dependency_graph;
 pub use self::dependency_graph::DependencyGraph;
+
+mod instance_wrapper;
+pub use self::instance_wrapper::InstanceWrapper;
+
+mod context;
+pub use self::context::Context;
